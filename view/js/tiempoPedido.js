c7006eab349aea790940ab6655e4f7e33f75c76e
KALLM <guillen4473@gmail.com>
2022-05-01 01:15:53 -0600

    Se ayudo a daniel para lograr obtener los productos seleccionados

--- view/js/tiempoPedido.js
+++ view/js/tiempoPedido.js
@@ -1,48 +1,50 @@
 console.log("Hola mundo")
+
+let produtosSeleccionados = [];
+
 document.getElementById('Pedidos').addEventListener('change', (event) => {
     let pedido = $('.Pedidos option:selected').val();
 });
-document.getElementById('btn_agregar').addEventListener('click', function () {
+document.getElementById('btn_agregar').addEventListener('click', function() {
     let pedido = $('.Pedidos option:selected').val();
     document.getElementById('txt_pedido').innerHTML += '<p id="producto_seleccionado">' + pedido + '</p>'
-    console.log(pedido)
+    produtosSeleccionados.push(pedido);
 })
 
-document.getElementById('btn_eliminar').addEventListener('click', function () {
+document.getElementById('btn_eliminar').addEventListener('click', function() {
     $('#txt_pedido').children().last().remove()
-    $('#txt_pedido').children().text();
-    console.log($('#txt_pedido').children().text().toLowerCase());
+    produtosSeleccionados.splice(produtosSeleccionados.length - 1);
 })
 
-document.getElementById('btn_simular').addEventListener('click', function () {
+let tiempo_preparacion = 0;
+let precio = 0;
+
+document.getElementById('btn_simular').addEventListener('click', function() {
     let servidor = $('.Servidores option:selected').val();
     document.getElementById('txt_servidor').innerHTML = '<p id="producto_seleccionado">' + servidor + '</p>'
-    console.log(pedido)
-    $.ajax({
-        type: "GET",
-        url: "http://localhost:3000/producto/",
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: function (data) {
-            $('#Table > tbody').empty();
-            $.each(data, function (i, item) {
-                var rows =
-                    "" +
-                    "" + item.id + "" +
-                    "" + item.Nombres + "" +
-                    "" + item.Cargo + "" +
-                    "" + item.Dpto + "" +
-                    "";
-                $('#Table > tbody').append(rows);
-            });
-            console.log(data);
-        },
-        failure: function (data) {
-            alert(data.responseText);
-        },
-        error: function (data) {
-            alert(data.responseText);
-        }
-    });
+    for (let i = 0; i < produtosSeleccionados.length; i++) {
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:3000/producto/" + produtosSeleccionados[i],
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function(data) {
+                $('#Table > tbody').empty();
+                $.each(data, function(i, item) {
+                    tiempo_preparacion = item.tiempo_preparacion;
+                    precio = item.precio;
+                    console.log(tiempo_preparacion);
+                    console.log(precio);
+                });
+                console.log(data);
+            },
+            failure: function(data) {
+                alert(data.responseText);
+            },
+            error: function(data) {
+                alert(data.responseText);
+            }
+        });
+    }
 
 })
